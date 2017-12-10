be2a8b2b8845fa1fc4c8d18b45fa03658bf48b6e
Ruslan Kovalov <kovalov.ruslan@gmail.com>
2017-12-10 18:09:20 +0000

    Create 'lucky' template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
@@ -16,8 +17,6 @@ class LuckyController
     {
         $number = mt_rand(0, 100000000000000);
 
-        return new Response(
-            '<html><body> Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', ['number' => $number]);
     }
 }
